e7592079925a92a874c8f51461f87fd255076783
neodev2 <neodev2@gmail.com>
2018-03-29 23:35:21 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -45,3 +45,39 @@ function getDatetime(){
 function capitalize(str){
 	return str.charAt(0).toUpperCase() + str.substring(1);
 }
+
+
+
+/* cookies */
+
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function deleteCookie(name) {   
+    setCookie(name, '', 0);
+}
+
+function deleteAllCookies(){
+	var cookies = document.cookie.split(";");
+	for(var i=0; i<cookies.length; i++){
+		deleteCookie(cookies[i].split("=")[0]);
+	}
+}
